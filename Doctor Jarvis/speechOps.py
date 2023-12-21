879ea4d0d209812ab00ba607473934488f897d78
Divyank Singh <singhdivyank7@gmail.com>
2023-12-22 01:06:41 +0530

    fix: dependency version issues and module object responses

--- Doctor Jarvis/speechOps.py
+++ Doctor Jarvis/speechOps.py
@@ -1,10 +1,10 @@
 import os
+import time
 
 import speech_recognition as sr
 
 from gtts import gTTS
-
-from typing import Any
+from pygame import mixer
 
 
 class Transcribe:
@@ -15,24 +15,30 @@ class Transcribe:
         # initialise microphone
         self.mic = sr.Microphone()
     
-    def get_text(self) -> Any:
+    def get_text(self):
         """
         convert audio from microphone to text 
         """
 
-        with self.mic as source:
-            self.recognizer.adjust_for_ambient_noise(source=source)
-            audio = self.recognizer.listen(source=source)
-            transcribe = self.recognizer.recognize_google(audio_data=audio, 
-                                                          language=self.language
-                                                          )
-            return transcribe
+        try:
+            with self.mic as source:
+                self.recognizer.adjust_for_ambient_noise(source=source)
+                audio = self.recognizer.listen(source=source)
+                transcribed_txt = self.recognizer.recognize_google(
+                    audio_data=audio, 
+                    language=self.language
+                )
+                print(transcribed_txt)
+                return transcribed_txt
+        except sr.RequestError:
+            return "NO INTERNET CONNECTION"
 
 
 class Translate:
     def __init__(self, txt_msg: str, language: str):
         self.txt_msg = txt_msg
         self.language = language
+        self.mixer = mixer.init()
     
     def text_to_speech(self):
         """
@@ -41,12 +47,17 @@ class Translate:
         """
 
         # name of audio file
-        audio_file = os.path.join(os.getcwd(), 
-                                  f"{os.getenv('audio_file')}.mp3"
-                                  )
+        audio_file = os.path.join(
+            os.getcwd(), 
+            f"{os.getenv('audio_file')}.mp3"
+        )
         # generate audio using module
-        speech = gTTS(self.txt_msg, lang=self.language)
+        speech = gTTS(text=self.txt_msg, lang=self.language)
         # save to .mp3 file
-        speech.save(audio_file)
-        # play mp3 file
-        os.system(command=f"mpg321 {audio_file}")
+        speech.save(savefile=audio_file)
+        # play the mp3 file
+        self.mixer.music.load(audio_file)
+        self.mixer.music.play()
+        # wait for music to finish playing
+        while self.mixer.music.get_busy():
+            time.sleep(secs=1)
