98c7f7031776490cd8077940da28b6ed681c7aa4
guest <other@Arrties-MacBook-Air.local>
2024-02-21 09:44:48 +0530

    fix: unicode error

--- Machine Learning/Diabetes Predictor/main.py
+++ Machine Learning/Diabetes Predictor/main.py
@@ -3,22 +3,22 @@ import pickle
 
 import gradio as gr
 
-def diabetesApp(glucose: float, insulin: float, bmi: float, age: int) -> str:
+def diabetesApp(glucose: str, insulin: str, bmi: str, age: str) -> str:
     """
     make prediction using trained classifier model
 
     Params:
-        glucose (float): numeric value for glucose
-        insulin (float): numeric value for insulin
-        bmi (float): numeric value for bmi
-        age (int): numeric value for age
+        glucose (str): numeric value for glucose received from UI
+        insulin (str): numeric value for insulin received from UI
+        bmi (str): numeric value for bmi received from UI
+        age (str): numeric value for age received from UI
     
     Returns:
         prediction made by classifier model
     """
 
     classifier = pickle.load(file=open(file='classifier.pkl', mode='rb'))
-    prediction = classifier.predict([[glucose, insulin, bmi, age]])[0]
+    prediction = classifier.predict([[float(glucose), float(insulin), float(bmi), int(age)]])[0]
     return "Diabetic" if prediction else "Undiabetic"
 
 
@@ -53,6 +53,6 @@ if __name__ == '__main__':
     )
     ui.launch(
         server_name='127.0.0.1',
-        server_port=8000,
+        server_port=8001,
         share=False
     )
